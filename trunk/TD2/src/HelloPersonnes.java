d7e8392ba32b80c8f021c99ce32d164f3c6e7106
anthonygourraud@gmail.com <anthonygourraud@gmail.com@2da6ff0a-8bc0-0fcd-50f5-14fb31f5f937>
2014-11-07 08:01:44 +0000

    DDD

--- trunk/TD2/src/HelloPersonnes.java
+++ trunk/TD2/src/HelloPersonnes.java
@@ -7,7 +7,7 @@ public class HelloPersonnes {
 	 */
 	public static void main(String[] args) {
 		Personne[] Tab = new Personne[3];
-		Tab[0] = new Personne("JeanMichel", 45) ;
+		Tab[0] = new Personne("JeanMichelDDD", 45) ;
 		Tab[1] = new Etudiant("Paul" , 22, 4.5f);
 		Tab[2] = new Enseignant("Pierre", 54, 35);
 		for(int i = 0; i<2 ; ++i) {

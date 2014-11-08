a6c881b73ce72950073d5ea519de9e8acf616ae5
anthonygourraud@gmail.com <anthonygourraud@gmail.com@2da6ff0a-8bc0-0fcd-50f5-14fb31f5f937>
2014-11-08 23:23:26 +0000

    arret sam 9/11

--- trunk/TD-Anthony/src/ComunicaSystem.java
+++ trunk/TD-Anthony/src/ComunicaSystem.java
@@ -7,29 +7,32 @@ import communication.*;
 
 public class ComunicaSystem {
 
-	
+
 	public static void main(String[] args) {
-		FileReader a2bReader ;
-		FileReader b2aReader ;
-		FileWriter a2bWriter ;
-		FileWriter b2aWriter ;
-		
+//		FileReader a2bReader ;
+//		FileReader b2aReader ;
+//		FileWriter a2bWriter ;
+//		FileWriter b2aWriter ;
+
 		try {
-			a2bReader = new FileReader("A2B.txt");
-			a2bWriter = new FileWriter("A2B.txt");			
-			b2aReader = new FileReader("B2A.txt");
-			b2aWriter = new FileWriter("B2A.txt");
-			
-			Comunica fenetre1 = new Comunica (new BufferedReader(a2bReader), new BufferedWriter(b2aWriter));
-			Comunica fenetre2 = new Comunica (new BufferedReader(b2aReader), new BufferedWriter(a2bWriter));				
+//			a2bReader = new FileReader("A2B.txt");
+//			a2bWriter = new FileWriter("A2B.txt");			
+//			b2aReader = new FileReader("B2A.txt");
+//			b2aWriter = new FileWriter("B2A.txt");
+
+			Comunica fenetre1 = new Comunica (null, null);
+			Comunica fenetre2 = new Comunica (null, null);
+			ComunicaTCPServer server = new ComunicaTCPServer() ; 
+			ComunicaTCPClient client1 = new ComunicaTCPClient(fenetre1, "localhost") ; 
+			ComunicaTCPClient client2 = new ComunicaTCPClient(fenetre2, "localhost") ; 
 		}
 		catch(Exception e) {
 			System.err.println("Le système a recontré une erreur : " )  ;
 			e.printStackTrace();
 		}
-		
-	
+
+
 	}	
-	
-	
+
+
 }
--- trunk/TD-Anthony/src/communication/ComunicaTCPClient.java
+++ trunk/TD-Anthony/src/communication/ComunicaTCPClient.java
@@ -3,11 +3,31 @@ package communication;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
+import java.net.*;
+import java.io.*;
 
-public class ComunicaTCPClient extends Comunica {
+public class ComunicaTCPClient{
 
-	public ComunicaTCPClient(BufferedReader reader, BufferedWriter writer)throws IOException {
-		super(reader, writer);
+	private final static int port = 9632;
+	private Socket socketClient ;  
+	private BufferedReader bfin ; 
+	private BufferedWriter bfout ;
+	private Comunica fenetreCom ; 
+	
+	public ComunicaTCPClient(Comunica fenetreCom, String adresse)throws IOException {
+		try {
+			socketClient = new Socket(InetAddress.getByName(adresse), port);
+			System.out.println("Connexion avec : "+socketClient.getInetAddress());
+ 
+			bfin  =  new BufferedReader(new InputStreamReader(socketClient.getInputStream()));
+			bfout =  new BufferedWriter(new BufferedWriter( new OutputStreamWriter(socketClient.getOutputStream())));
+	        
+			this.fenetreCom = fenetreCom ; 
+			this.fenetreCom.setReader(bfin) ; 
+			this.fenetreCom.setWriter(bfout) ; 
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
-
 }
